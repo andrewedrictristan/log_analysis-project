da041d88a85c9aa7fc136ba96da59253c1bac0f0
vagrant <vagrant@vagrant.vm>
2018-10-02 15:51:08 +0000

    last commit

--- log_analysis_db.py
+++ log_analysis_db.py
@@ -5,16 +5,19 @@ import psycopg2
 
 def get_favorite_articles(c):
     # TOP 3 POPULAR ARTICLES
-    c.execute("select author_slugs.title,vs_article.total_visits from author_slugs,vs_article "
+    c.execute("select author_slugs.title,vs_article.total_visits "
+              "from author_slugs,vs_article "
               "where vs_article.slug_path=author_slugs.slug limit 3;")
     return c.fetchall()
 
 
 def get_favorite_authors(c):
     # MOST POPULAR AUTHORS
-    c.execute("select author_slugs.author_name,sum(vs_article.total_visits) as most_fav_author "
+    c.execute("select author_slugs.author_name,sum(vs_article.total_visits) "
+              "as most_fav_author "
               "from author_slugs,vs_article where "
-              "author_slugs.slug=vs_article.slug_path group by author_slugs.author_name order by "
+              "author_slugs.slug=vs_article.slug_path "
+              "group by author_slugs.author_name order by "
               "most_fav_author desc;")
     return c.fetchall()
 
@@ -23,7 +26,9 @@ def get_error_requests(c):
     # DATE AND ERROR RATE THAT HIGHER THAN 1%
     c.execute("select rate,date "
               "from("
-              "select cast((error_requests.error_req*100)/request_per_day.number_of_request as decimal(10,2))"
+              "select cast((error_requests.error_req*100)"
+              "/request_per_day.number_of_request "
+              "as decimal(10,2))"
               " as rate,request_per_day.date "
               "from error_requests,request_per_day "
               "where request_per_day.date=error_requests.date)as rate_error "
@@ -44,13 +49,14 @@ def main():
     # Looping for writing in file
     t = "1. What are the most popular three articles of all time? \r\n"
     t += "\r\n".join("%s - %s Views " % (author, views)
-                   for author, views in get_favorite_articles(c))
+                     for author, views in get_favorite_articles(c))
     t += "\r\n\r\n2. Who are the most popular article authors of all time?\r\n"
     t += "\r\n".join("%s - %s Views " % (title, visits)
-                   for title, visits in get_favorite_authors(c))
-    t += "\r\n\r\n3. On which days did more than 1% of requests lead to errors?\r\n"
+                     for title, visits in get_favorite_authors(c))
+    t += "\r\n\r\n3. On which days did more than 1% of " \
+         "requests lead to errors?\r\n"
     t += "\r\n".join("%s%% - %s " % (rate, date.strftime('%d, %b %Y'))
-                   for rate, date in get_error_requests(c))
+                     for rate, date in get_error_requests(c))
 
     # Writing into file and save
     file.write(t)
@@ -58,6 +64,7 @@ def main():
 
     # Disconnect the database connection
     db.close()
+
 
 if __name__ == '__main__':
     # Running the main() function
